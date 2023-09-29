8fff8ec8c97e37b0248fa8104f1137fc7317ba88
Jyati-Agarwal <142707948+Jyati-Agarwal@users.noreply.github.com>
2023-09-29 23:34:08 +0530

    Question 9

---
+++ A1Q9.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class A1Q9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+
+        int gcd = gcd(n1, n2);
+        int lcm = n1 * n2 / gcd;
+
+        System.out.println(gcd);
+        System.out.println(lcm);
+    }
+
+    private static int gcd(int n1, int n2) {
+        if (n2 == 0) {
+            return n1;
+        }
+
+        return gcd(n2, n1 % n2);
+    }
+}
